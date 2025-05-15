8c2c9134d98273dc8b6b963aea6d7604acd611ed
rparvate15 <132704121+rparvate15@users.noreply.github.com>
2025-05-15 12:11:14 -0400

    made circle look cleaner and added animation in list view

--- TasksNHabits/Views/HabitListView.swift
+++ TasksNHabits/Views/HabitListView.swift
@@ -44,20 +44,29 @@ struct HabitListView: View {
                                 .foregroundStyle(.purple)
                             
                             ZStack {
-                                
                                 Circle()
-                                    .stroke(lineWidth: 2)
+                                    .stroke(style: StrokeStyle (
+                                        lineWidth: 2,
+                                        lineCap: .round,
+                                        lineJoin: .round
+                                        )
+                                    )
                                     .foregroundStyle(.gray)
                                     .opacity(0.2)
                                     .fixedSize()
                                 
                                 Circle()
                                     .trim(from: 0, to: CGFloat(habit.currentAmount) / CGFloat(habit.totalAmount))
-                                    .stroke(lineWidth: 2)
+                                    .stroke(stroke(style: StrokeStyle (
+                                        lineWidth: 2,
+                                        lineCap: .round,
+                                        lineJoin: .round
+                                    )
+                            ))
                                     .foregroundStyle(.purple)
                                     .fixedSize()
                                     .rotationEffect(.degrees(-90))
-                                
+                                    .animation(.easeInOut(.duration(0.3)), value: habit.currentAmount)
                             }
                             
                             Text("\(habit.frequency)")
